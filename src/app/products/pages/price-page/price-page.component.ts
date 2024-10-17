13763b90ec784b4cf73af3f73aa93e2a79ac0300
Jhonny <jhonnycgarcia@gmail.com>
2024-10-16 22:49:05 -0500

    ngOnDestro

--- src/app/products/pages/price-page/price-page.component.ts
+++ src/app/products/pages/price-page/price-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'products-price-page',
@@ -10,8 +11,14 @@ export class PricePageComponent implements OnInit, OnChanges, OnDestroy {
   @Input()
   public price: number = 0;
 
+  public interval$?: Subscription;
+
   ngOnInit(): void {
     console.log('HIJO - ngOnInit');
+
+    this.interval$ = interval(1000).subscribe((value) => {
+      console.log('HIJO Tick:', value);
+    });
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -20,6 +27,7 @@ export class PricePageComponent implements OnInit, OnChanges, OnDestroy {
   }
   ngOnDestroy(): void {
     console.log('HIJO - ngOnDestroy');
+    this.interval$?.unsubscribe();
   }
 
 }
